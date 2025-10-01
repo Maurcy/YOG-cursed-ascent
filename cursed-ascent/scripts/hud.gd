4f46cb09cd49e01b4cc68b1d164f68da6f2c7299
Mauricio Samson <450322@student.fontys.nl>
2025-10-01 14:10:29 +0200

    made a basic HUD with a timer, starts the moment you jump

---
+++ cursed-ascent/scripts/hud.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+@onready var levelTimerDisplay: Label = $Control/levelTimerDisplay
+@onready var game_manager: Node = %gameManager
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float):
+	levelTimerDisplay.text = "Remaining time: " + str(snapped(game_manager.remainingLevelTime, 0.01))
