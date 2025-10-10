1dd3fd0459230723b4d97de0d3f80b793280a2c1
Mauricio <samsonmauricio@gmail.com>
2025-10-10 08:57:42 +0200

    added rooms, bug at game manager w dictionary

--- cursed-ascent/scripts/game_manager.gd
+++ cursed-ascent/scripts/game_manager.gd
@@ -4,10 +4,20 @@ var remainingLevelTime
 var timeUp = false;
 @onready var levelTimer: Timer = $levelTimer
 
+#
+const ROOM_WIDTH := 160
+const ROOM_HEIGHT := 160
+
+var room_variants = {}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	levelTimer.wait_time = levelTime
 	remainingLevelTime = levelTime
+	
+	preload_rooms()
+	var level_layout = LevelGenerator.generate_level()
+	generate_level(level_layout);
 
 # Called every frame. 'delta' is the e6lapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -20,3 +30,47 @@ func _process(delta: float) -> void:
 
 func _on_level_timer_timeout() -> void:
 	timeUp = true
+
+
+func preload_rooms():
+	var base_path = "res://rooms/"
+	var folder_map = {
+		LevelGenerator.normal_room: "normal_rooms",
+		LevelGenerator.hard_room: "hard_rooms",
+		LevelGenerator.dangerous_room: "dangerous_rooms",
+		LevelGenerator.treasure_room: "treasure_rooms"
+	}
+
+	for type in folder_map.keys():
+		var path = base_path + folder_map[type]
+		var dir = DirAccess.open(path)
+		if dir:
+			dir.list_dir_begin()
+			var file = dir.get_next()
+			while file != "":
+				if file.ends_with(".tscn"):
+					room_variants[type].append(load(path + "/" + file))
+				file = dir.get_next()
+			dir.list_dir_end()
+
+
+func get_random_room_scene(room_type: int) -> PackedScene:
+	var list = room_variants.get(room_type, [])
+	if list.size() == 0:
+		push_error("No scenes found for room type: %d" % room_type)
+		return null
+	return list[randi() % list.size()]
+
+func generate_level(grid: Array):
+	for y in range(grid.size()):
+		for x in range(grid[y].size()):
+			var room_type = grid[y][x]
+			if room_type == null:
+				continue
+				
+			var room_scene = get_random_room_scene(room_type)
+			
+			if room_scene:
+				var instance = room_scene.instantiate()
+				instance.position = Vector2(x * ROOM_WIDTH, y * ROOM_HEIGHT)
+				add_child(instance)
--- cursed-ascent/scripts/level_generator.gd
+++ cursed-ascent/scripts/level_generator.gd
@@ -1,27 +1,38 @@
-extends Node
-var left_border = 30
-var right_border = 1120
-var size = 0;
-var height = 6;
-var grid := []
+# class_name lets it be accessible globally in case I want to delete the level_generator node
+# not sure what the best practice is rn, kind of just experimenting here
+class_name LevelGenerator
 
+# the room types
 const spawn_room = 0
 const normal_room = 1
 const hard_room = 2
 const dangerous_room = 3
 const treasure_room = 4
 
-func _ready() -> void:
-	size =  (right_border - left_border) / 3
-
-func generate_level() -> void:
-	grid.clear()
+static func generate_level():
+	var grid := []
+	var height = 6;
+	
+	var rooms = [normal_room, hard_room, dangerous_room]
 	
 	for y in range(height):
 		grid.append([null, null, null])	
+		
+		# if the y == 0, then  it won't spawn dangerous rooms
+		var max = 2
 		if y == 0:
-			var room = randi_range(1,2)
-			pass
-		else:
-			pass
-	pass
+			max = 1
+
+		for x in range(3):
+			var room = randi_range(0, max)
+			grid[y][x] = rooms[room]
+		
+		# checks if there aren't any normal rooms in row
+		# needs to be expanded so it doesnt overwrite spawn room
+		if normal_room not in grid[y]:
+			var x = randi_range(0, 2)
+			grid[y][x] = normal_room
+			
+	# treasure check still needs to be implemented
+	
+	return grid
