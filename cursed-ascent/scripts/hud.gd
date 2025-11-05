f09eded60d650a37db47a0ebb4a813d7c0e363ca
Mauricio Samson <450322@student.fontys.nl>
2025-11-05 15:20:50 +0100

    Anchored timer to center and changed size, changed level timer to 15 seconds for testing purposes, text now changes color when it reaches 30 and 10 seconds, dislays milisecond when below one minute

--- cursed-ascent/scripts/hud.gd
+++ cursed-ascent/scripts/hud.gd
@@ -10,6 +10,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
-	levelTimerDisplay.text = "Remaining time: " + str(snapped(game_manager.remainingLevelTime, 0.01))
+	# if there's only less than a minute, starts displaying miliseconds
+	if game_manager.remainingLevelTime <= 60:
+		levelTimerDisplay.text = "%.1f" % game_manager.remainingLevelTime
+	else:
+		levelTimerDisplay.text = "%.0f" % game_manager.remainingLevelTime
+	
+	if game_manager.remainingLevelTime <= 30:
+		levelTimerDisplay.add_theme_color_override("font_color", Color(1.0,1.0,0.0,1.0))
+	
+	if game_manager.remainingLevelTime <= 10:
+		levelTimerDisplay.add_theme_color_override("font_color", Color(1.0,0.0,0.0,1.0))
+	
 	if game_manager.timeUp:
 		levelTimerDisplay.text = "Time's up!"
