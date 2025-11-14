c0498cd879b740de4f4ba42bd879f7b24d306369
Mauricio Samson <450322@student.fontys.nl>
2025-11-14 10:54:03 +0100

    worked on the visual aspect, uses necrosans for all text (didnt touch upgrades yet), new tileset, new titlescreen, fixed platofmr being too transparent

--- cursed-ascent/scripts/hud.gd
+++ cursed-ascent/scripts/hud.gd
@@ -4,16 +4,19 @@ extends CanvasLayer
 @onready var game_start_text: Label = $Control/gameStartText
 @onready var gameover: Label = $Control/Gameover
 @onready var gameoverlevels: Label = $Control/Gameoverlevels
+@onready var jumptoretarts: Label = $Control/jumptoretarts
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GameManager.timeup.connect(_on_time_up)
 	gameover.visible = false;
 	gameoverlevels.visible = false;
+	jumptoretarts.visible = false
 
 func _on_time_up() -> void:
 	gameover.visible = true;
 	gameoverlevels.visible = true;
+	jumptoretarts.visible = true
 	gameoverlevels.text = "You've completed " + str(GameManager.completedLevels) + " levels"
 
 # time handler.
